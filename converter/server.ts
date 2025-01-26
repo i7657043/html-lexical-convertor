108af8c857ee7051fdc5c6d0d579c4017b7f852f
Jordan Griffiths <jordan_griff2012@hotmail.com>
2025-01-26 13:09:22 +0000

    dockerfile working with converter

--- converter/server.ts
+++ converter/server.ts
@@ -1,17 +1,29 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import { $generateNodesFromDOM } from '@lexical/html';
-import { $getRoot, createEditor, $getSelection } from 'lexical';
-import { JSDOM } from 'jsdom';
+import rateLimit from 'express-rate-limit';
+import { logger } from './src/logger';
+import { convertHtmlToLexical } from './src/convertHtmlToLexical';
 
 const app = express();
-const port =  3000;
+const port = 3000;
 
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 3,
+  message: 'Too many requests from this IP, please try again after 1 minute',
+});
+
+app.use(limiter);
 app.use(bodyParser.json());
 
 app.post('/', (req: Request, res: Response) => {  
   var htmlString = req.body.htmlString;
   var fieldName = req.body.fieldName;
+
+  var ip = req.headers['x-forwarded-for'];
+  logger.info(`Request from IP: ${ip}`);
 
   var json = convertHtmlToLexical(htmlString, fieldName)
 
@@ -19,29 +31,12 @@ app.post('/', (req: Request, res: Response) => {
   });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  if (process.env.NODE_ENV === 'development') {
+    logger.info(`Server running in dev mode at http://localhost:${port}`);
+  }
+  else
+  {
+    logger.info(`Server running on port: ${port}`);
+  }
 });
 
-function convertHtmlToLexical(htmlString: string, fieldName: string): { [key: string]: any } {
-  const editor = createEditor();
-
-  editor.update(
-    () => {
-      const nodes = $generateNodesFromDOM(
-        editor,
-        new JSDOM(htmlString).window.document
-      );
-
-      $getRoot().select();
-      const selection = $getSelection();
-      selection?.insertNodes(nodes);
-    },
-    { discrete: true }
-  );
-
-  const lexicalJson = editor.getEditorState().toJSON();
-
-  return {
-    [fieldName]: lexicalJson,
-  };
-}
